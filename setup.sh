04c03f14ef282434f7cb577197f0d8a18ce775a0
argans99 <167126095+argans99@users.noreply.github.com>
2025-12-02 18:17:23 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ az extension add -n ml -y
 #RESOURCE_GROUP_NAME="cloudml"
 GROUP="default_group"
 LOCATION="eastus"
-WORKSPACE="test_workspace_azure_ml"
+WORKSPACE="GL-MLworkspace-az-august"
 # </set_variables>
 
 
